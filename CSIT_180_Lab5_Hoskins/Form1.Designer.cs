3468017f05c90efc3b474f7e89a640d888cae130
Katie Riley <katie.ann.riley@hotmail.com>
2016-04-12 07:03:30 -0700

    first commit

---
+++ CSIT_180_Lab5_Hoskins/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace CSIT_180_Lab5_Hoskins
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.elevationTextBox = new System.Windows.Forms.TextBox();
+            this.quitButton = new System.Windows.Forms.Button();
+            this.airportNameLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.airportName = new System.Windows.Forms.TextBox();
+            this.tempTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // elevationTextBox
+            // 
+            resources.ApplyResources(this.elevationTextBox, "elevationTextBox");
+            this.elevationTextBox.Name = "elevationTextBox";
+            // 
+            // quitButton
+            // 
+            this.quitButton.BackColor = System.Drawing.Color.MediumAquamarine;
+            resources.ApplyResources(this.quitButton, "quitButton");
+            this.quitButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.quitButton.Name = "quitButton";
+            this.quitButton.UseVisualStyleBackColor = false;
+            this.quitButton.Click += new System.EventHandler(this.quitButton_Click);
+            // 
+            // airportNameLabel
+            // 
+            resources.ApplyResources(this.airportNameLabel, "airportNameLabel");
+            this.airportNameLabel.ForeColor = System.Drawing.Color.Teal;
+            this.airportNameLabel.Name = "airportNameLabel";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.ForeColor = System.Drawing.Color.Teal;
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.ForeColor = System.Drawing.Color.Teal;
+            this.label3.Name = "label3";
+            // 
+            // airportName
+            // 
+            resources.ApplyResources(this.airportName, "airportName");
+            this.airportName.Name = "airportName";
+            // 
+            // tempTextBox
+            // 
+            resources.ApplyResources(this.tempTextBox, "tempTextBox");
+            this.tempTextBox.Name = "tempTextBox";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumAquamarine;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.recordDataButton_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.Color.Teal;
+            this.label1.Name = "label1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aquamarine;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.quitButton);
+            this.Controls.Add(this.tempTextBox);
+            this.Controls.Add(this.elevationTextBox);
+            this.Controls.Add(this.airportName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.airportNameLabel);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox elevationTextBox;
+        private System.Windows.Forms.Button quitButton;
+        private System.Windows.Forms.Label airportNameLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox airportName;
+        private System.Windows.Forms.TextBox tempTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CSIT_180_Lab5_Hoskins/Form1.cs
@@ -0,0 +1,159 @@
+﻿/*
+*  Kathryn Hoskins
+*  
+*
+* Develop a Windows form application that enables the
+*   user to enter values for airport name, Celsius temperature,
+*   and elevation. Have a multi line message box that outputs 
+*   the airports name, celsius temperature, fahrenheit temperature, 
+*   and elevation 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSIT_180_Lab5_Hoskins
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void recordDataButton_Click(object sender, EventArgs e)
+        {
+
+            //int[] boo = new int [10]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Airport myAirport;              
+            string myAirportName = " ";
+            double myCelsiusTemp = 0.0;
+            double myAirportElev = 0.0;
+
+            //max values used for error checking
+            double tempMax = 50;    
+            double tempMin = -50;
+            double elevationMax = 12000;
+            double elevationMin = -300;
+
+            //the message that will be displayed in the message box
+            string message = " ";
+            
+            //setting the variables with values from the correct textbox
+            myAirportName = airportName.Text;
+            myCelsiusTemp = Convert.ToDouble(tempTextBox.Text);
+            myAirportElev = Convert.ToDouble(elevationTextBox.Text);
+
+            //error checking to set the message box to either display an error message telling 
+            //the user what went wrong or displaying the correct information
+            if (myAirportName == "")
+            {
+                message = "That is not a valid Airport name";
+            }
+            else if (myCelsiusTemp < tempMin || myCelsiusTemp > tempMax)
+            {
+                message = "That is not a valid temperature \r\nThe temperature must be above -50c and below 50c";
+            }
+            else if (myAirportElev < elevationMin || myAirportElev > elevationMax)
+            {
+                message = "That is not a valid elevation \r\nThe elevation needs to be above -300 feet and below 12000 feet";
+            }
+            //all the inputs were valid so now we can set up our airport object and
+            //set the message 
+            else
+            {
+                myAirport = new Airport(myAirportName, myCelsiusTemp, myAirportElev);
+
+                message = "The Airports name is: " + myAirport.getAirportName() + "\r\n";
+                message += "The Airports celsius temperature is: " + Convert.ToString( myAirport.getCelsiusTem()) + "\r\n";
+                message += "The Airports fahrenheit temperature is: " + Convert.ToString(myAirport.celsiusToFahrenheit()) + "\r\n";
+                message += "The Airports elevation is: " + Convert.ToString(myAirport.getElevation()) + "\r\n";
+
+            }
+            //showing either an error message or the correct output depending on the user input
+            MessageBox.Show(message);
+        }
+        
+        //quits the application
+        private void quitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Control myControl = new Control();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+    public class Airport {
+        private string airportName;
+        private double celsiusTemp;
+        private double airportElevation;
+
+        /* public Airport(string theName, double theTemp, double theElevation)
+        *   constructor
+        *   Parameters: string - the name of the airport
+        *               double - the teprature in celsius 
+        *               double - the elevation               
+        *   sets all variables
+        */
+        public Airport(string theName, double theTemp, double theElevation)
+        {
+            airportName = theName;
+            celsiusTemp = theTemp;
+            airportElevation = theElevation;
+        }
+
+        /* public double celsiusToFahrenheit()
+        *   Parameters: NONE               
+        *   Returns:    double - the temprature in fahrenheit
+        */
+        public double celsiusToFahrenheit()
+        {
+            double fahrenheitTemp = 0.0;
+
+            celsiusTemp *= 1.8;
+            fahrenheitTemp = celsiusTemp + 32;
+
+            return fahrenheitTemp;
+        }
+
+        /*the getter for the class so we can output into the message box*/
+        public string getAirportName()
+        {
+            return airportName;
+        }
+
+        public double getCelsiusTem()
+        {
+            return celsiusTemp;
+        }
+        
+        public double getElevation()
+        {
+
+           
+            return airportElevation;
+        }
+    }
+}
